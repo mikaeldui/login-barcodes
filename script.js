ce2dcddf47a4aec9fb4fcbd040a2d44c3b3e6d2c
Mikael Dúi Bolinder <github@bolinder.uk>
2025-07-10 23:33:01 +0300

    Added DotCode and Han Xin

--- script.js
+++ script.js
@@ -8,15 +8,43 @@ function getText() { // encode chars >127 for reader conformity
     return unescape(encodeURIComponent(username.value + "\t" + password.value + "\n")); // unicode to UTF-8
 }
 function draw() {
+
+    (async () => {
+        console.log('1')
+        await getHtml()  
+        console.log('2')
+    })();
+
     draw_qr();
     draw_aztec();
+    draw_dotcode();
+    draw_hanxin();
 }
 
-function draw_qr() {
+async function draw_qr() {
     var r = quickresponse(username.value + "\t" + password.value + "\n", "A", -3);
     qr.src = toGif(r, s, 0);
+    return Promise.
 }
-function draw_aztec() {
+async function draw_aztec() {
     var r = aztec(getText(), 23);  
     az.src = toGif(r, s, 0);
+}
+async function draw_dotcode() {
+    let canvas = document.createElement('canvas');
+    bwipjs.toCanvas(canvas, {
+        bcid:        'dotcode',       // Barcode type
+        text:        username.value + "\t" + password.value + "\n",    // Text to encode
+        scale:       30,               // 3x scaling factor
+    });
+    dc.src = canvas.toDataURL('image/png');
+}
+async function draw_hanxin() {
+    let canvas = document.createElement('canvas');
+    bwipjs.toCanvas(canvas, {
+        bcid:        'hanxin',       // Barcode type
+        text:        username.value + "\t" + password.value + "\n",    // Text to encode
+        scale:       30,               // 3x scaling factor
+    });
+    hx.src = canvas.toDataURL('image/png');
 }
