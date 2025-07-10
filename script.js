8e4eee203bbafb0e5a9f7dbd0ab50e507254ee14
Mikael Dúi Bolinder <github@bolinder.uk>
2025-07-10 23:34:51 +0300

    Fix

--- script.js
+++ script.js
@@ -8,29 +8,21 @@ function getText() { // encode chars >127 for reader conformity
     return unescape(encodeURIComponent(username.value + "\t" + password.value + "\n")); // unicode to UTF-8
 }
 function draw() {
-
-    (async () => {
-        console.log('1')
-        await getHtml()  
-        console.log('2')
-    })();
-
     draw_qr();
     draw_aztec();
     draw_dotcode();
     draw_hanxin();
 }
 
-async function draw_qr() {
+function draw_qr() {
     var r = quickresponse(username.value + "\t" + password.value + "\n", "A", -3);
     qr.src = toGif(r, s, 0);
-    return Promise.
 }
-async function draw_aztec() {
+function draw_aztec() {
     var r = aztec(getText(), 23);  
     az.src = toGif(r, s, 0);
 }
-async function draw_dotcode() {
+function draw_dotcode() {
     let canvas = document.createElement('canvas');
     bwipjs.toCanvas(canvas, {
         bcid:        'dotcode',       // Barcode type
@@ -39,7 +31,7 @@ async function draw_dotcode() {
     });
     dc.src = canvas.toDataURL('image/png');
 }
-async function draw_hanxin() {
+function draw_hanxin() {
     let canvas = document.createElement('canvas');
     bwipjs.toCanvas(canvas, {
         bcid:        'hanxin',       // Barcode type
